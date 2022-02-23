9e9bd7ea4a6ba0304b9eeae512e89baa95ab44ef
Alexander Kryuchkov <alkryuchkov@mail.ru>
2022-02-23 20:25:11 +1100

    Ex.3 - done

--- KryuchkovAiOS5_HW3_3.playground/Contents.swift
+++ KryuchkovAiOS5_HW3_3.playground/Contents.swift
@@ -7,3 +7,22 @@ import UIKit
 
 
 //ЗАДАЧА №3
+print("\nЗАДАЧА №3\n")
+
+class Parent {
+    func about() {
+        print("About Parent")
+    }
+}
+
+class Child: Parent {
+    override func about() {
+        super.about()
+        
+        print("About Child")
+    }
+}
+
+// Проверка
+let childOne = Child()
+childOne.about()
