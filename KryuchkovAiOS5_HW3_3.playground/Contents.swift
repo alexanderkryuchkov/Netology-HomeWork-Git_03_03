a6419917852622dca56108a496fb0882bb83d44b
Alexander Kryuchkov <alkryuchkov@mail.ru>
2022-02-23 20:30:52 +1100

    Ex.2 - done

--- KryuchkovAiOS5_HW3_3.playground/Contents.swift
+++ KryuchkovAiOS5_HW3_3.playground/Contents.swift
@@ -4,6 +4,28 @@ import UIKit
 
 
 //ЗАДАЧА №2
+print("\nЗАДАЧА №2\n")
+
+class testOverload {
+    
+    func printValueType(_ value: Int) {
+        print("Функция выводит переменную типа - \(type(of: value))")
+    }
+    
+    func printValueType(_ value: String) {
+        print("Функция выводит переменную типа - \(type(of: value))")
+    }
+    
+    func printValueType(_ value: Bool) {
+        print("Функция выводит переменную типа - \(type(of: value))")
+    }
+}
+
+let testValue = testOverload()
+testValue.printValueType(5)
+testValue.printValueType("Тест")
+testValue.printValueType(true)
+
 
 
 //ЗАДАЧА №3
